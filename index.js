dce432ee4162be465a88bfd38f0e70b8bdfdfca7
Paul <43885283+Crystalflxme@users.noreply.github.com>
2019-07-12 19:20:38 +0800

    Music bot updates

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const Discord = require("discord.js");
 const mongoose = require("mongoose")
-const EXP = require("./models/exp.js/index.js")
+const EXP = require("./models/exp.js")
 const { CommandHandler } = require("djs-commands")
-const botSecrets = require("./bot-secrets.json")
+const botSecrets = require("./../bot-secrets.json")
 const botConfig = require("./bot-config.json")
 const CH = new CommandHandler({
     folder: __dirname + "/commands/",
     prefix: [";"]
 });
 var bot = new Discord.Client()
+bot.music = require("discord.js-musicbot-addon");
 
-mongoose.connect("mongodb://localhost:27017/EXP", {
-    useNewUrlParser: true
-})
+//mongoose.connect("mongodb://localhost:27017/EXP", {
+    //useNewUrlParser: true
+//})
+bot.music.start(bot, {
+    youtubeKey: botSecrets.YTAPI
+});
 
 bot.on("message", (message) => {
     if (message.channel.type === "dm") return
@@ -22,30 +26,30 @@ bot.on("message", (message) => {
     let cmd = CH.getCommand(command)
     if (cmd) {
         try {
-            cmd.run(bot,message,args)
+            cmd.run(bot, message, args)
         } catch(e) {
             console.log(e)
         }
-    } else if (!cmd) {
-        let expToAdd = Math.floor(Math.random() * 10)
-        print(expToAdd)
-        EXP.findOne({
-            userID: message.author.id,
-            serverID: message.guild.id
-        }, (err, exp) => {
-            if (err) console.log(err)
-            if (!exp) {
-                const newEXP = new EXP({
-                    userID: message.author.id,
-                    serverID: message.guild.id,
-                    exp: expToAdd
-                })
-                newEXP.save().catch(err => console.log(err))
-            } else {
-                exp.exp = exp.exp + expToAdd
-                newEXP.save().catch(err => console.log(err))
-            }
-        })
+    // } else if (!cmd) {
+    //     let expToAdd = Math.floor(Math.random() * 10)
+    //     print(expToAdd)
+    //     EXP.findOne({
+    //         userID: message.author.id,
+    //         serverID: message.guild.id
+    //     }, (err, exp) => {
+    //         if (err) console.log(err)
+    //         if (!exp) {
+    //             const newEXP = new EXP({
+    //                 userID: message.author.id,
+    //                 serverID: message.guild.id,
+    //                 exp: expToAdd
+    //             })
+    //             newEXP.save().catch(err => console.log(err))
+    //         } else {
+    //             exp.exp = exp.exp + expToAdd
+    //             newEXP.save().catch(err => console.log(err))
+    //         }
+    //     })
     }
 });
 
